14069bf43523724c9d533c6b0473dab8f5e2ad5d
Georges <georges.boughantous@gmail.com>
2024-07-30 12:00:45 +0000

    website

---
+++ js/navbar.js
@@ -0,0 +1,29 @@
+$(function () {
+    $("#1990-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/ZrInldACPRc?si=AAVNdWR8QIFXtMaV");
+    });
+    $("#1994-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/IEUiDmJPbzw?si=E8iU4GwBZ1RYsyOO");
+    });
+    $("#1998-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/dZojzlALVZE?si=EFI6stn3SC4F-rBj");
+    });   
+    $("#2002-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/c3frBZ4tI9U?si=N2TK_qVLImMU1B5v");
+    });
+    $("#2006-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/LWsKObhKrco?si=-ZXo1W3o5OU_5m47");
+    });
+    $("#2010-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/WSmWT9fSW1I?si=Zd1M8lRkntlT9JQb");
+    });
+    $("#2014-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/5YIRjt2Jjwo?si=03k4u8XfDZSR4NTU");
+    });
+    $("#2018-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/3MLCpvbn6bQ?si=SIqtpLDCUcgfylxk");
+    });
+    $("#2022-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/cVLLo5_q_4E?si=aaLXzsTekERyf-vW");
+    });
+});
