469ac90a5adb40aab33162785294fea4de0d54ed
Georges Bou Ghantous <georges.boughantous@gmail.com>
2026-04-16 20:34:00 +1000

    links updated

--- js/navbar.js
+++ js/navbar.js
@@ -2,49 +2,70 @@ $(function () {
     $("#home").click(function () {
         $("#Display-frame").attr("src", "images/footballer.gif");
     }); 
+    $("#1930-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/0g29HtvTzMQ?si=r8PJeCfsCj65zNFZ");
+    });
+    $("#1934-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/KXNH6xRqeQY?si=Ww2qAT_kEef8wHot");
+    });
+    $("#1938-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/958C8NiGOug?si=MeJPo3JcwOuWH-fs");
+    });
+    $("#1950-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/CD3YR6c_rLA?si=urmSHG3gvschbVsL");
+    });
+    $("#1954-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/8TVRWROuFjo?si=y_LGlW9BssI5dqcp");
+    });
+    $("#1958-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/CFf17nA9kTI?si=E49_ygLDpD19F1Cf");
+    });
+    $("#1962-link").click(function () {
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/csfddHxCcmc?si=hXLLqatNpmesd56F");
+    });
     $("#1966-link").click(function () {
-        $("#Display-frame").attr("src", "https://www.youtube.com/embed/p-rydGeXKH8?si=Q2fes6bxd6fHvNSB");
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/iyVKHalAZLk?si=hqtshnxwDUn7ovYb");
     });
     $("#1970-link").click(function () {
-        $("#Display-frame").attr("src", "https://www.youtube.com/embed/zW5K0n-zuzc?si=1HFuSPEznYD6SpO_");
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/eFb0WIfv074?si=nMQ2uDfLCqqh5rW7");
     });
     $("#1974-link").click(function () {
-        $("#Display-frame").attr("src", "https://www.youtube.com/embed/OQ0zG6a2bWg?si=7_dmiuj6kgQFGrd0");
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/yyyEc7VSSCQ?si=ahNkXbrjD1gCjfN8");
     });
     $("#1978-link").click(function () {
-        $("#Display-frame").attr("src", "https://www.youtube.com/embed/lVDcxSlqU_0?si=0AVOM2tfga8j_DSS");
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/a8Ns2D81JSg?si=TG1HASc3uQvroTH1");
     });
     $("#1982-link").click(function () {
-        $("#Display-frame").attr("src", "https://www.youtube.com/embed/t3wGVv65rdw?si=8nMErYdUXfXlAtB1");
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/x7SCOi5ALKo?si=bJzCdsVwdSpA1E-l");
     });
     $("#1986-link").click(function () {
-        $("#Display-frame").attr("src", "https://www.youtube.com/embed/T8y7DgY_wNg?si=SPcMDCs3fkrNGjDy");
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/PKWpzsjqLSE?si=C1-U3jUaMI6m6un6");
     });
     $("#1990-link").click(function () {
-        $("#Display-frame").attr("src", "https://www.youtube.com/embed/ZrInldACPRc?si=AAVNdWR8QIFXtMaV");
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/VAxRvnI32jk?si=CIPd80VhfTWMV72n");
     });
     $("#1994-link").click(function () {
-        $("#Display-frame").attr("src", "https://www.youtube.com/embed/IEUiDmJPbzw?si=E8iU4GwBZ1RYsyOO");
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/gSVATlkI3mQ?si=UNcp17nosk9Nyjfh");
     });
     $("#1998-link").click(function () {
-        $("#Display-frame").attr("src", "https://www.youtube.com/embed/dZojzlALVZE?si=EFI6stn3SC4F-rBj");
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/GClHm71DesQ?si=o2TFM-qWgsIFI07V");
     });   
     $("#2002-link").click(function () {
         $("#Display-frame").attr("src", "https://www.youtube.com/embed/c3frBZ4tI9U?si=N2TK_qVLImMU1B5v");
     });
     $("#2006-link").click(function () {
-        $("#Display-frame").attr("src", "https://www.youtube.com/embed/LWsKObhKrco?si=-ZXo1W3o5OU_5m47");
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/_yiRD0gk4yc?si=lRLdD4a3ic9f_0Vq");
     });
     $("#2010-link").click(function () {
-        $("#Display-frame").attr("src", "https://www.youtube.com/embed/WSmWT9fSW1I?si=Zd1M8lRkntlT9JQb");
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/HcKsTy0Ie0o?si=gAT3wS6ABz6iQDCC");
     });
     $("#2014-link").click(function () {
-        $("#Display-frame").attr("src", "https://www.youtube.com/embed/5YIRjt2Jjwo?si=03k4u8XfDZSR4NTU");
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/V4RF-TqWvoI?si=jK8FBZf-h5q-OaII");
     });
     $("#2018-link").click(function () {
-        $("#Display-frame").attr("src", "https://www.youtube.com/embed/3MLCpvbn6bQ?si=SIqtpLDCUcgfylxk");
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/eRVKBQW1qIM?si=87WNCLfOusws2Z7j");
     });
     $("#2022-link").click(function () {
-        $("#Display-frame").attr("src", "https://www.youtube.com/embed/cVLLo5_q_4E?si=aaLXzsTekERyf-vW");
+        $("#Display-frame").attr("src", "https://www.youtube.com/embed/YbMH5TPQEb4?si=0fYulJTcfDw23CqK");
     });
 });
